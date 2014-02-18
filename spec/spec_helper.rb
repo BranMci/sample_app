9e94e85e4a002bbf0b62d19d0e0200e3f868aff1
CrixsB <brandon@brandonmcinnis.com>
2014-02-17 17:59:37 -0800

    Fixed spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,14 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+  # Checks for pending migrations before tests are run.
+  # If you are not using ActiveRecord, you can remove this line.
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
-  
-  # ## Mock Framework
+    # ## Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
     #
@@ -41,8 +42,7 @@ Spork.prefork do
     # the seed, which is printed after each run.
     #     --seed 1234
     config.order = "random"
-    config.include Capybara::DSLend
-
+    config.include Capybara::DSL
   end
 end
 
@@ -50,5 +50,3 @@ Spork.each_run do
   # This code will be run each time you run your specs.
 
 end
-
-
